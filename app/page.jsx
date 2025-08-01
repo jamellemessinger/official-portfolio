2500a62c84c3cc7a56d3c736a460cff7ca2a03b2
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-01 15:15:48 -0600

    wrap h1 in a parent component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <h1>Portfolio Under Construction :)</h1>
+    <div className="home">
+      <h1>Portfolio Under Construction :)</h1>
+    </div>
   );
 }
