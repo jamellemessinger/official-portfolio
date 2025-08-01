248552a5d8c7e72c370dab8f0d5a8a1af065d880
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-01 16:08:08 -0600

    add border bottom and color match link dividers

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <nav className="navbar flex justify-between p-2">
-      <img className="md:hidden rounded-full" src="/images/TMlogo.JPG" width="40"></img>
+    <nav className="navbar flex justify-between p-2 border-b border-gray-300">
+      <img
+        className="md:hidden rounded-full"
+        src="/images/TMlogo.JPG"
+        width="40"
+      ></img>
       <div className="hidden md:block">Jamelle Messinger</div>
       <div className="flex gap-2 items-center">
         <Link href="/">Home</Link>
