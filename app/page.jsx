e39620e8502cd363858d7dff62db8d022d068028
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-02 17:22:59 -0600

    add section for future css styling

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="home">
-      <h1>Jamelle Messinger</h1>
-      <p>Full-Stack Developer | IT Professional</p>
-      {/* download resume component */}
+      <section>
+        <h1>Jamelle Messinger</h1>
+        <p>Full-Stack Developer | IT Professional</p>
+        {/* download resume component */}
+      </section>
     </div>
   );
 }
