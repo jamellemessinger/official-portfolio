1b8c35917d770aaab18326714b03126f6e135309
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-02 17:17:17 -0600

    add name and short desc. Layout for resume component

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="home">
-      <h1>Portfolio Under Construction :)</h1>
+      <h1>Jamelle Messinger</h1>
+      <p>Full-Stack Developer | IT Professional</p>
+      {/* download resume component */}
     </div>
   );
 }
