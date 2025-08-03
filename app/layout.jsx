d36d4fb580d681dc29e54ac6f846e4a217b604a5
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-02 18:06:22 -0600

    remove defualt font variables

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,17 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+// import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import NavBar from './components/NavBar';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+// font variables created by next
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+// const geistMono = Geist_Mono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// });
 
 export const metadata = {
   title: 'Jamelle Messinger',
@@ -21,7 +22,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f5f5f5]`}
+        // ${geistSans.variable} ${geistMono.variable} may use these to import fonts later mayber?
+        className={`antialiased bg-[#f5f5f5]`}
       >
         <NavBar />
         <main>{children}</main>
