3b29236a974f9842d3106d352c4bf96e5eab5172
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-03 17:47:41 -0600

    add p5background component to home

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+import P5Background from './components/P5Background';
 
 export default function Home() {
   return (
     <div className="home">
+      <P5Background />
       <section className="hero">
         <h1>Jamelle Messinger</h1>
         <p>Full-Stack Developer | IT Professional</p>
