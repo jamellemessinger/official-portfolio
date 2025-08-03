4fbec14f080b1649ddf58ce45d95c4849a0cfd31
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-03 17:33:16 -0600

    create a walker class. rename folder

---
+++ app/p5/Walker.js
@@ -0,0 +1,50 @@
+class Walker {
+  constructor() {
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+    stroke(
+      this.#randomNum255(),
+      this.#randomNum255(),
+      this.#randomNum255(),
+      75
+    );
+  }
+
+  offScreen() {
+    if (this.x > width || this.y > height || this.x < 0 || this.y < 0) {
+      return true;
+    }
+    return false;
+  }
+
+  #randomNum255() {
+    return Math.floor(Math.random() * 255);
+  }
+
+  show() {
+    point(this.x, this.y);
+    strokeWeight(10);
+  }
+
+  move() {
+    let decision = Math.floor(Math.random() * 4);
+
+    switch (decision) {
+      case 0:
+        this.x += 10;
+        break;
+      case 1:
+        this.x -= 10;
+        break;
+      case 2:
+        this.y += 10;
+        break;
+      case 3:
+        this.y -= 10;
+        break;
+      default:
+        this.x += 5;
+        break;
+    }
+  }
+}
