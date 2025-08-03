a15bfbf651881e2552d421aeb521e6dafa56bb1d
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-03 15:35:06 -0600

    auto formatting

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Projects() {
   return (
-    <div className='projects'>
+    <div className="projects">
       <h1>Projects page coming soon!</h1>
     </div>
-  )
+  );
 }
