22105a53c760f9d5b56294fc502892ee13e9d65d
Jamelle Messinger <jamswoof914@gmail.com>
2025-08-03 18:23:02 -0600

    refactor walker class to take the p5 instance as an argument

--- app/p5/Walker.js
+++ app/p5/Walker.js
@@ -1,10 +1,11 @@
 export default class Walker {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.x = Math.random() * width;
-    this.y = Math.random() * height;
-    stroke(
+  constructor(p5Instance) {
+    this.p5Instance = p5Instance;
+    this.width = this.p5Instance.windowWidth;
+    this.height = this.p5Instance.windowHeight;
+    this.x = Math.random() * this.width;
+    this.y = Math.random() * this.height;
+    this.p5Instance.stroke(
       this.#randomNum255(),
       this.#randomNum255(),
       this.#randomNum255(),
@@ -13,7 +14,12 @@ export default class Walker {
   }
 
   offScreen() {
-    if (this.x > width || this.y > height || this.x < 0 || this.y < 0) {
+    if (
+      this.x > this.width ||
+      this.y > this.height ||
+      this.x < 0 ||
+      this.y < 0
+    ) {
       return true;
     }
     return false;
@@ -24,8 +30,8 @@ export default class Walker {
   }
 
   show() {
-    point(this.x, this.y);
-    strokeWeight(10);
+    this.p5Instance.point(this.x, this.y);
+    this.p5Instance.strokeWeight(10);
   }
 
   move() {
